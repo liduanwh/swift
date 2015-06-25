9001eebff5dc20f9cc5a05a42b880480a81c56a7
李端 <23589953@qq.com>
2015-06-25 10:07:35 +0800

    initial

---
+++ HelloIOS/HelloIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloIOS
+//
+//  Created by 李端 on 15/6/25.
+//  Copyright (c) 2015年 李端. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
